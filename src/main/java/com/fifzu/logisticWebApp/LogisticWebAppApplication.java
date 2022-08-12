751e314253a56ad80877b48369e9bf0e87d4c53a
Fifzu <friedrich.albrecht@yahoo.com>
2022-08-12 13:07:09 +0200

    Initial commit

---
+++ src/main/java/com/fifzu/logisticWebApp/LogisticWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.fifzu.logisticWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticWebAppApplication.class, args);
+	}
+
+}
